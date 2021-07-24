dd4d50bd4ff368c877c73bd4d2b0f92beabe7e0e
Hann1234 <christopherjhannigan@gmail.com>
2021-07-24 16:39:29 -0500

    added imports to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import Details from '../Details/Details'
+import AddMovies from '../AddMovies/AddMovies'
 
 function App() {
   return (
